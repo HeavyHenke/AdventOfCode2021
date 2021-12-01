9e93edcb7e1dfe1e322b9fd5f9dda5348a61d078
Henrik Hellenborg <henrik@hellenborg.com>
2021-12-01 07:57:22 +0100

    Day 1

---
+++ AoC2022/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AoC2022;
+
+
+Console.WriteLine("Hello, World!");
+
+DateTime start = DateTime.Now;
+string result = new Day1().B()?.ToString() ?? " ";
+DateTime stop = DateTime.Now;
+
+Console.WriteLine("It took " + (stop - start).TotalSeconds);
+
+WindowsClipboard.SetText(result);
+Console.WriteLine(result);
