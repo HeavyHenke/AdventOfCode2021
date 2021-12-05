7c45fde720e0b65ac625d460a606ebb6b8cdfe02
Henrik Hellenborg <henrik@hellenborg.com>
2021-12-05 11:52:13 +0100

    Döpt om foldern

---
+++ AoC2021/Day5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MoreLinq.Extensions;
+
+namespace AoC2022
+{
+    class Day5
+    {
+        public object A()
+        {
+            return 0;
+        }
+
+        public object B()
+        {
+            return 0;
+        }
+    }
+}
