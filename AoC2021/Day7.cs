4d00e40e0d4414e1f5711897c7c5c14ba94cab9f
Henrik Hellenborg <henrik@hellenborg.com>
2021-12-07 08:21:05 +0100

    Day 7

---
+++ AoC2021/Day7.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC2021
+{
+    public class Day7
+    {
+        public object A()
+        {
+            //var data = "16,1,2,0,4,2,7,1,2,14".Split(',').Select(int.Parse).ToArray();
+            var data = File.ReadAllLines("Day7.txt")[0].Split(',').Select(int.Parse).ToArray();
+
+            var bestFuelCost = int.MaxValue;
+            for (int i = data.Min(); i < data.Max(); i++)
+            {
+                var fuelCost = data.Sum(d => Math.Abs(d-i));
+                if(fuelCost > bestFuelCost)
+                    break;
+                if (fuelCost < bestFuelCost)
+                    bestFuelCost = fuelCost;
+            }
+            
+            return bestFuelCost;
+        }
+        
+        public object B()
+        {
+            //var data = "16,1,2,0,4,2,7,1,2,14".Split(',').Select(int.Parse).ToArray();
+            var data = File.ReadAllLines("Day7.txt")[0].Split(',').Select(int.Parse).ToArray();
+
+            var bestFuelCost = int.MaxValue;
+            for (int i = data.Min(); i < data.Max(); i++)
+            {
+                var fuelCost = data.Sum(d => FuelCost(Math.Abs(d-i)));
+                if(fuelCost > bestFuelCost)
+                    break;
+                if (fuelCost < bestFuelCost)
+                    bestFuelCost = fuelCost;
+            }
+            
+            
+            return bestFuelCost;
+        }
+
+        private static int FuelCost(int dist)
+        {
+            var cost = 0;
+            while (dist > 0)
+            {
+                cost += dist;
+                dist--;
+            }
+
+            return cost;
+        }
+    }
+}
